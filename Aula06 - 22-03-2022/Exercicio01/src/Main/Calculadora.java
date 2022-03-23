f72b0bf77cd0d62070126f9320a7b038b1428163
Estefanio-santos <mr.estefanio@hotmail.com>
2022-03-23 17:51:36 -0300

    Conclusão do exercício 01 da aula 06

--- Aula06 - 22-03-2022/Exercicio01/src/Main/Calculadora.java
+++ Aula06 - 22-03-2022/Exercicio01/src/Main/Calculadora.java
@@ -10,4 +10,16 @@ public double subtracao(double n1, double n2) {
 	return n1 - n2;
 }
 
+public double multiplicar(double n1, double n2) {
+	return n1 * n2;
+}
+
+public double dividir(double n1, double n2) {
+if(n2 == 0) {
+	return -1;
+} else {
+	return n1 / n2;
+}
+}
+
 } // fim da classe
--- Aula06 - 22-03-2022/Exercicio01/src/Tests/CalculadoraTest.java
+++ Aula06 - 22-03-2022/Exercicio01/src/Tests/CalculadoraTest.java
@@ -2,32 +2,47 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Main.Calculadora;
 
 public class CalculadoraTest {
+private Calculadora c;
 
-@
+@BeforeEach
+public void setup() {
+	c = new Calculadora();
+}
 
 @Test
 	public void deveRetornarASomaDosNumeros() {
-		Calculadora c = new Calculadora();
-		double resultado = c.somar(10, 20);
+	double resultado = c.somar(10, 20);
 		assertEquals(30, resultado);
 	}
 
 @Test
 	public void deveRetornarASubtracaoDosNumeros() {
-		Calculadora c = new Calculadora();
 		double resultado = c.subtracao(20, 10);
 		assertEquals(10, resultado);
 	}
 
-public void deveRetornar() {
-	double resultado = c.multiplicacao();
+@Test
+public void deveRetornarAMultiplicacaoDosNumeros() {
+	double resultado = c.multiplicar(2, 3);
+	assertEquals(6, resultado);
+}
+
+@Test
+public void deveRetornarADivisaoDosNumerosQuandoDivisorDiferenteDeZero() {
+	double resultado = c.dividir(4, 2);
 	assertEquals(2, resultado);
 }
 
+@Test
+public void deveRetornarValorNegativoQuandoDivisorIgualZero() {
+	double resultado = c.dividir(0, 0);
+	assertEquals(-1, resultado);
+}
 
 } // fim da classe
